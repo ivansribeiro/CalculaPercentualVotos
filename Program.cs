789f29df848f48052959f204bc52421bb2724084
Comelli <ivansrib@gmail.com>
2021-06-17 02:12:47 -0300

    Projeto Piloto de Médias de Votos

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Votos_Eleitores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Votos_Validos percValidos = new Votos_Validos();
+            Votos_Validos percBrancos = new Votos_Validos();
+            Votos_Validos percNulos = new Votos_Validos();
+
+
+
+            Console.WriteLine("Informe o tatal de Eleitores");
+            string totEleitores = Console.ReadLine();
+            double totalEleitores = Convert.ToDouble(totEleitores);
+
+            Console.WriteLine("Informe o total de votos validos");
+            string totValidos = Console.ReadLine();
+            double totalValidos = Convert.ToDouble(totValidos);
+
+            Console.WriteLine("Informe o total de votos brancos");
+            string totBrancos = Console.ReadLine();
+            double totalBrancos = Convert.ToDouble(totBrancos);
+
+            Console.WriteLine("Informe o total de votos nulos");
+            string totNulos = Console.ReadLine();
+            double totalNulos = Convert.ToDouble(totNulos);
+
+
+            double percentualValidos = percValidos.percValidos(totalEleitores, totalValidos);
+            double percentualBrancos = percBrancos.percBrancos(totalEleitores, totalBrancos);
+            double percentualNulos = percNulos.percNulos(totalEleitores, totalNulos);
+
+            Console.WriteLine("Votos Validos foram :"+ percentualValidos);
+            Console.WriteLine("Votos Brancos foram :" + percentualBrancos);
+            Console.WriteLine("Votos Nulos foram :" + percentualNulos);
+
+
+
+        }
+    }
+}
---
+++ Votos_Validos.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Votos_Eleitores
+{
+    class Votos_Validos
+    {
+       
+        public Votos_Validos() { } //criacao da classe
+
+        public double percValidos(double totalEleitores, double validos)
+        {
+            double percentualValidos;
+
+            percentualValidos = validos  / totalEleitores;
+            return percentualValidos;
+        }
+
+        public double percBrancos(double totalEleitores, double votosBrancos)
+        {
+            double percentualBrancos;
+
+            percentualBrancos = votosBrancos / totalEleitores;
+
+            return percentualBrancos;
+        }
+
+        public double percNulos(double totalEleitores, double votosNulos)
+        {
+            double percentualVotosNulos;
+
+            percentualVotosNulos = votosNulos / totalEleitores;
+
+            return percentualVotosNulos;
+        }
+
+
+    }
+}
